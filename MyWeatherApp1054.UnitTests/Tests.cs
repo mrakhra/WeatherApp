9a5689bd02e94ebdf33cd9419c67adfe00b0faa4
Manbir Rakhra <manbir.s.rakhra@gmail.com>
2019-08-03 14:23:07 +1000

    Adding Unit tests projects.

---
+++ MyWeatherApp1054.UnitTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyWeatherApp1054.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PipelinesTests()
+        {
+            Assert.True(true);
+        }
+    }
+}
